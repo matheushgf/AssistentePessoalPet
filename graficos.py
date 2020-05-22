0298b00045b8d38c908dca23a092f115d4f2a0fe
RenanRhoads <61717442+RenanRhoads@users.noreply.github.com>
2020-05-21 23:31:13 -0300

    Add files via upload

---
+++ graficos.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+
+def graficos():
+
+    #Utilizando matplotlib
+
+    #Define os valores de teste no gráfico
+    plt.plot([15, 30, 60, 90], [5.25, 4.89, 5.3, 6])
+    plt.ylabel('Peso do PET (KG)')
+    plt.xlabel('Dias')
+    plt.title('Dias')
+
+    plt.show()
+#graficos()
+
