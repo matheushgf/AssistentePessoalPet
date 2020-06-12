ecba2313897a30923aa5c8428a87a8b5b2e0e818
Jonathan Henrique Gusmão dos Santos <59443974+jsantos40@users.noreply.github.com>
2020-06-12 16:41:40 -0300

    Update Registro.py
    Versão para testes.

--- Registro.py
+++ Registro.py
@@ -5,69 +5,76 @@ from tkinter import *
 #from get_tables_field import *
 #from insert import *
 
-
 registro_peso = []  #Salvar no banco.
-registro_racao = []  #Salvar no banco.
-'''
-def reg():
-    str(reg_pes.get())
-    registro_peso.append(reg_pes.get())
+registro_racao = []  #
+reg_rac_min = 0
 
-    str(reg_rac)
-    registro_racao.append(reg_rac.get())
-'''
 def validad():
+
+    #Fazer laço para caso os dados sejam letras
+
+    global reg_rac_min
+    reg_rac_min = float(reg_pes.get()) * 0.03
 
     if not reg_pes.get().isnumeric() and len(reg_pes.get()) == 0 and str(reg_pes.get()) == '':
 
-        txt_peso["text"] = 'Dado inválido, digite novamente.'
+        lb_peso["text"] = 'Dado inválido, digite novamente.'
 
     else:
 
-        str(reg_pes.get())
-        registro_peso.append(reg_pes.get())
+        if not reg_rac.get().isnumeric() and len(reg_rac.get()) == 0 and str(reg_rac.get()) == '':
 
-    if not reg_rac.get().isnumeric() and len(reg_rac.get()) == 0 and str(reg_rac.get()) == '':
+            lb_racao["text"] = 'Dado inválido, digite novamente.'
 
-        pass
+        elif float(reg_rac.get()) <= reg_rac_min:  # Condição de invalidez por minimo.
 
-    else:
+            rac["text"] = 'A quantidade de ração não pode ser menos que o mínimo indicado!'
 
-        str(reg_rac)
-        registro_racao.append(reg_rac.get())
+        else:
 
+            lb_peso["text"] = ''
+            lb_racao["text"] = ''
+            save["text"] = 'Dados salvos com sucesso.'
 
+            str(reg_rac)
+            registro_racao.append(reg_rac.get())
+
+            str(reg_pes.get())
+            registro_peso.append(reg_pes.get())
+
+#Interface gráfica
 janela = Tk()
 
-#Campos de registro
+#Campos de registro.
 reg_pes = Entry(janela, width=50)
 reg_pes.place(x=40, y=130)
 
 reg_rac = Entry(janela, width=50)
 reg_rac.place(x=40, y=220)
 
-#Texto de cada campo
+#Textos de cada campo.
 tex_pes = Label(janela, text="Peso:")
 tex_pes.place(x=40, y=110)
 
 tex_reg = Label(janela, text="Qtde Ração:")
-tex_reg.place(x=40, y=200)
+tex_reg.place(x=40, y=195)
 
-#Mensagem de erro para o usuário.
+#Mensagens de erro ou informativa para o usuário.
 lb_peso = Label(janela, text='')
 lb_peso.place(x=225, y=155)
 
 lb_racao = Label(janela, text='')
 lb_racao.place(x=225, y=245)
 
-legenda = Label(janela, texto= 'Nos informe o peso e a ração do Pet')
-legenda.place(x=200, y=50)
+save = Label(janela, text= '')
+save.place(x=155, y=360)
+
+rac = Label(janela, text='')
+rac.place(x=35, y=280)
 
 #Botão de salvamento
 bt = Button(janela, width=20, text="OK", command=validad)
 bt.place(x=150, y=400)
-
-
 
 janela.title("Registro")
 janela.geometry("500x500+300+300")
