07f13e504da5c206cd808a5777b5d87d3b1b3ee7
jsantos40 <jsantos.ti@outlook.com>
2020-06-26 16:30:41 -0300

    atualização

--- registro_peso.py
+++ registro_peso.py
@@ -6,6 +6,80 @@ from recognizer import recognizer
 #import datetime
 
 def registro_peso():
+    '''Validação dos dados----------------------------------------------------------------------------------------------'''
+
+    #Função secundária que chama função primária.
+    def valida(entrada):
+
+        if entrada == '' or entrada == None:
+            valid = False
+            while valid is not True:
+                #messenger["text"] = 'Não ouvi, repita novamente'
+                print('Não ouvi, repita novamente')
+                entrada = recognizer()
+                if entrada != '' or entrada != None:
+                    valida_num(entrada)
+                    valid = True
+                    return True
+        else:
+            valida_num(entrada)
+            return True
+
+    #Função primária
+    def valida_num(value):
+        n = None
+
+        try:
+            entrada_d = value.split(' ')
+            numero = entrada_d[0]
+            #messenger["text"] = f'O Número informado foi {numero}'
+            print(f'O Número informado foi {numero}')
+            numero = numero.replace(',', '.')
+            #audio_peso.set(str(numero))   #Label de peso
+            print(numero)
+        except AttributeError:
+            #lb_peso["text"] = 'Valor inválido, repita'
+            print('Valor inválido, repita')
+        except ValueError:
+            #lb_peso["text"] = 'Valor não foi dito corretamente.'
+            print('Valor não foi dito corretamente.')
+        except:
+            #lb_peso["text"] = 'Valor inválido'
+            print('Valor inválido')
+
+    #Comandos feitos por voz, com chamada dinâmica para a Lebal
+    confirmado = False
+    while confirmado is not True:
+
+        campos = {
+            'Peso do pet': {'validacao': valida, 'mensagem': 'Qual o peso do Pet hoje'}
+        }
+
+        for campo in campos.keys():
+            valido = False
+            while valido is not True:
+                dados = campos[campo]
+                #messenger["text"] = dados['mensagem']
+                print(dados['mensagem'])
+                texto = recognizer()
+
+                try:
+                    metodo = dados['validacao']
+                    valido = metodo(texto)
+                except:
+                    #messenger["text"] = 'Erro no método de validação'
+                    print('Erro no método de validação')
+                    break
+
+        if valido:
+            #messenger["text"] = 'Deseja confirmar sim ou não'
+            print('Deseja confirmar sim ou não')
+            texto = recognizer()
+
+            if texto == 'Sim':
+                valido = True
+                confirmado = True
+
     '''Interface gráfica------------------------------------------------------------------------------------------------'''
 
     janela = Tk()
@@ -31,71 +105,5 @@ def registro_peso():
     janela.geometry("500x500+300+300")
     janela.mainloop()
 
-    '''Validação dos dados----------------------------------------------------------------------------------------------'''
-
-    #Função secundária que chama função primária.
-    def valida(entrada):
-
-        if entrada == '' or entrada == None:
-            valid = False
-            while valid is not True:
-                messenger["text"] = 'Não ouvi, repita novamente'
-                entrada = recognizer()
-                if entrada != '' or entrada != None:
-                    valida_num(entrada)
-                    valid = True
-                    return True
-        else:
-            valida_num(entrada)
-            return True
-
-    #Função primária
-    def valida_num(value):
-        n = None
-
-        try:
-            entrada_d = value.split(' ')
-            numero = entrada_d[0]
-            messenger["text"] = f'O Número informado foi {numero}'
-            numero = numero.replace(',', '.')
-            audio_peso.set(str(numero))   #Label de peso
-        except AttributeError:
-            lb_peso["text"] = 'Valor inválido, repita'
-        except ValueError:
-            lb_peso["text"] = 'Valor não foi dito corretamente.'
-        except:
-            lb_peso["text"] = 'Valor inválido'
-
-    #Comandos feitos por voz, com chamada dinâmica para a Lebal
-    confirmado = False
-    while confirmado is not True:
-
-        campos = {
-            'Peso do pet': {'validacao': valida, 'mensagem': 'Qual o peso do Pet hoje'}
-        }
-
-        for campo in campos.keys():
-            valido = False
-            while valido is not True:
-                dados = campos[campo]
-                messenger[""] = dados['mensagem']
-                texto = recognizer()
-
-                try:
-                    metodo = dados['validacao']
-                    valido = metodo(texto)
-                except:
-                    messenger["text"] = 'Erro no método de validação'
-                    break
-
-        if valido:
-            messenger["text"] = 'Deseja confirmar sim ou não'
-            texto = recognizer()
-
-            if texto == 'Sim':
-                valido = True
-                confirmado = True
-
 registro_peso()
 
-print('Saiu')
