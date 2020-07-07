b118da6ecc0984a68efe2ca52f241269ddf81478
WilliamAntoniazzi <william.antonaizzi@fatec.sp.gov.br>
2020-07-07 19:44:11 -0300

    Atualização scripts SQL base Assistente Pet

--- default_insert.sql
+++ default_insert.sql
@@ -1,9 +1,5 @@
 insert into mydb.tipo_alerta (categoria_alerta) values
-('Vacina'),
-('Banho/Tosa'),
-('Outros');
+('Vacina'), ('Banho/Tosa'), ('Outros'), ('Ração');
 
 insert into mydb.tipo_pet (categoria_tipo_pet) VALUES
-('Cachorro'),
-('Gato'),
-('Outros');
+('Cachorro'), ('Gato'), ('Outros');
