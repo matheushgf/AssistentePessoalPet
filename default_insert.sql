7d9e522067b6932b9489e52c4fcd8d251f23bdc0
RCLMacedo07 <62268869+RCLMacedo07@users.noreply.github.com>
2020-07-12 16:26:59 -0300

    Corrigindo conflitos

---
+++ default_insert.sql
@@ -0,0 +1,5 @@
+insert into mydb.tipo_alerta (categoria_alerta) values
+('Vacina'), ('Banho/Tosa'), ('Outros'), ('Ração');
+
+insert into mydb.tipo_pet (categoria_tipo_pet) VALUES
+('Cachorro'), ('Gato'), ('Outros');
---
+++ recognizer.py
@@ -0,0 +1,41 @@
+import speech_recognition
+import importlib
+
+command_lists = {'registro de ração':{'nome_funcao':'registro_racao','nome_arquivo':'registro_racao'},
+                 'registro de lembrete':{'nome_funcao':'lembrete_pet', 'nome_arquivo':'lembrete_pet'},
+                 'clima':{'nome_funcao':'weather_app', 'nome_arquivo':'weather_app'},
+                 'registro de peso':{'nome_funcao':'registro_peso', 'nome_arquivo':'registro_peso'},
+                 'registro de animal':{'nome_funcao':'registro_pet', 'nome_arquivo':'registro_pet'}
+                 }
+
+def recognizer():
+        try:
+                speech_recognizer = speech_recognition.Recognizer()
+
+                with speech_recognition.Microphone() as source:
+                        print("Ouvindo")
+                        audio = speech_recognizer.listen(source)
+
+                saida = speech_recognizer.recognize_google(audio, language='pt')
+                saida = saida.strip()
+                print("O texto reconhecido é:")
+                print(saida)
+
+                return saida
+        except:
+                return ""
+
+def command_verification(user_command):
+
+        if ( user_command in command_lists.keys()):
+                dados = command_lists[user_command]
+                module = importlib.import_module(dados['nome_arquivo'])
+                metodo = getattr(module, dados['nome_funcao'])
+
+                try:
+                        metodo()
+                        return 'Comando executado'
+                except:
+                        return 'Erro ao executar método'
+        else:
+                return 'Comando não encontrado'
