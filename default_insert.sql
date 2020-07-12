2812ec61a99d3ebffd56c9a8829b8b97d01a6780
RCLMacedo07 <62268869+RCLMacedo07@users.noreply.github.com>
2020-07-11 23:36:33 -0300

    Limpando a branch de arquivos repetidos e desnecessesários

--- default_insert.sql
+++
@@ -1,9 +0,0 @@
-insert into mydb.tipo_alerta (categoria_alerta) values
-('Vacina'),
-('Banho/Tosa'),
-('Outros');
-
-insert into mydb.tipo_pet (categoria_tipo_pet) VALUES
-('Cachorro'),
-('Gato'),
-('Outros');
